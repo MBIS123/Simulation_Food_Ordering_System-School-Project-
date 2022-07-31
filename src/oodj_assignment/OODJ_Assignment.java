6a6873ac5aa627bfeb352708eff295e7a66d608d
HAO <HAO@DESKTOP-CD7H71U>
2022-07-31 20:11:30 +0800

    testing1

---
+++ src/oodj_assignment/OODJ_Assignment.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oodj_assignment;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author HAO
+ */
+public class OODJ_Assignment {
+
+   
+    public static void main(String[] args) {
+        
+    UserInterface form =  new UserInterface();
+    form.setBackground(Color.yellow);
+    form.setVisible(true);
+  
+    }
+    
+}
