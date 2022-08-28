b522242e153b499c9fea370d5d97eb7a0ab9c769
HAO <HAO@DESKTOP-CD7H71U>
2022-08-28 09:32:09 +0800

    add Menu.java

---
+++ src/oodj_assignment/Menu.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodj_assignment;
+
+import java.awt.MenuItem;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author HAO
+ */
+public class Menu {
+    
+    File menuFile = new File("Menu.txt");
+
+    public Menu() {
+       
+    }
+    
+    String[][] load_Menu_Data (){
+        ArrayList<String> menuItem1;
+        try {
+             BufferedReader rd = new BufferedReader(new FileReader(menuFile));
+             ArrayList<String> menuAL = new ArrayList<String>();
+             String line;
+        while (( line = rd.readLine())!= null) {
+            menuAL.add(line);
+        }
+        rd.close();
+        String[] menuArray = menuAL.toArray(new String[0]);
+        String[][] menuArray2D = new String[menuArray.length][3];
+        for(int i=0; i< menuArray.length ; i++){
+            for(int j=0; j<3; j++){
+                menuArray2D[i][j] = menuArray[i].split(":")[j]; 
+            }
+        }
+        return menuArray2D;  
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }   
+}
